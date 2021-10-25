5b0814e002f26b07a19a959baf04a2d594df8ff0
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-25 07:09:54 -0700

    beach image now scaling with window size

--- src/components/game/Main.js
+++ src/components/game/Main.js
@@ -3,11 +3,13 @@ import beach from '../../assets/beach.jpeg';
 
 export default function Main() {
   function clickHandler(event) {
-    console.log(event);
+    const X = event.screenX;
+    const Y = event.screenY;
+    console.log(X, Y);
   }
   return (
-    <main id='main_marvel_image'>
-      <img onClick={clickHandler} src={beach} alt='beach' />
+    <main id='beach_container'>
+      <img id='beach_image' onClick={clickHandler} src={beach} alt='beach' />
     </main>
   );
 }
