26921b4cbd8598cabc206d1a056c821e721f9681
Spencer Knight <spencer.knightusn@gmail.com>
2021-10-26 13:22:22 -0700

    resize listner no longer creating infinite loop

--- src/components/game/Main.js
+++ src/components/game/Main.js
@@ -3,17 +3,25 @@ import beach from '../../assets/beach.jpeg';
 import './main.css';
 
 export default function Main() {
+  const [windowMeasurements, setWindowMeasurements] = useState({});
+  const [calculatedCoordinates, setCalculatedCoordinates] = useState({}); // calculated coordinates when mini_menu was placed
   const [top, setTop] = useState(0);
   const [left, setLeft] = useState(0);
   const [visibility, setVisibility] = useState(false);
-
-  // TODO create useEffet for when the window screen size changes
-
   const pop_up_style = {
     top: top,
     left: left,
     visibility: visibility ? 'visible' : 'hidden',
   };
+
+  // TODO create useEffect for when the window screen size changes
+  useEffect(() => {
+    // window resize event listener
+    window.addEventListener('resize', () => {
+      const { innerWidth, innerHeight } = window;
+      setWindowMeasurements({ innerWidth, innerHeight });
+    });
+  }, []);
 
   function COORDINATE_CALC(event) {
     const IMAGE = document.getElementById('beach_image');
@@ -35,28 +43,28 @@ export default function Main() {
     }
   }
 
-  function SET_POP_UP_COORDINATES(event) {
+  function SET_POP_UP_COORDINATES(event, coordinates) {
     setLeft(event.pageX - 16);
     setTop(event.pageY - 16);
+    setCalculatedCoordinates(coordinates);
   }
 
   function clickHandler(event) {
     const COORDINATES = COORDINATE_CALC(event);
     setVisibility((prevState) => !prevState);
-    SET_POP_UP_COORDINATES(event);
+    SET_POP_UP_COORDINATES(event, COORDINATES);
   }
 
   return (
     <>
       <div style={pop_up_style} id='pop_up_list'>
-        {/* <div id='pop_up_list'> */}
         <svg id='svg_container' viewBox='0 0 100 100'>
           <circle
             cx='50'
             cy='50'
             r='50'
             stroke='green'
-            stroke-width='15'
+            strokeWidth='15'
             fill='none'
           />
         </svg>
@@ -65,6 +73,8 @@ export default function Main() {
       <main id='beach_container'>
         <img id='beach_image' onClick={clickHandler} src={beach} alt='beach' />
       </main>
+      {console.log(windowMeasurements.innerHeight)}
+      {console.log(windowMeasurements.innerWidth)}
     </>
   );
 }
